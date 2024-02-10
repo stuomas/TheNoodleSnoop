64f18662cebb24c9df3efbe22e3ebcee1ee650f8
Tuomas Salokanto <tuomas.salokanto@gmail.com>
2024-02-10 21:58:02 +0200

    Mitigate paho-mqtt 2.0 breaking changes
    Take new API version into use later
    
    Signed-off-by: Tuomas Salokanto <tuomas.salokanto@gmail.com>

--- ml_api/main.py
+++ ml_api/main.py
@@ -67,7 +67,7 @@ class AsyncMqttClient:
         print("Got message payload: {}".format(msg.payload))
 
     async def main(self):
-        self.client = mqtt.Client(client_id=client_id)
+        self.client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION1, client_id=client_id)
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
 
